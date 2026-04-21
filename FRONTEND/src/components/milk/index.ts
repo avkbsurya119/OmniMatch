d3927f9c2baf6fdf4757f106e47c78e036194073
amrithasnidhi <amrithasnidhi1234@gmail.com>
2026-04-22 04:46:54 +0530

    complete index states

---
+++ FRONTEND/src/components/milk/index.ts
@@ -0,0 +1,7 @@
+export { default as MilkDonorRegistration } from "./MilkDonorRegistration";
+export { default as MilkShortageBoard } from "./MilkShortageBoard";
+export { default as MilkDonorCard } from "./MilkDonorCard";
+export { default as MilkMatchModal } from "./MilkMatchModal";
+export { default as MilkPassportTable } from "./MilkPassportTable";
+export { default as MilkHospitalDashboard } from "./MilkHospitalDashboard";
+export { default as MilkDonorRequests } from "./MilkDonorRequests";
